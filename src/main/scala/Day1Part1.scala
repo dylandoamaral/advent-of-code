9664ca2ec0bdad4cf844e53a68565f771ecce79d
Dylan Do Amaral <do.amaral.dylan@gmail.com>
2024-12-07 08:43:05 +0100

    feat: day 1 done

---
+++ src/main/scala/Day1Part1.scala
@@ -0,0 +1,15 @@
+import scala.io.Source
+import scala.util.Using
+
+object Day1Part1 {
+  def main(args: Array[String]): Unit = {
+    val input = Using(Source.fromResource("Day1.txt"))(_.getLines().toList).get
+
+    val locations = input.collect { case s"$left   $right" => (left.toInt, right.toInt) }
+    val leftLocations = locations.map(_._1).sorted
+    val rightLocations = locations.map(_._2).sorted
+    val answer = leftLocations.zip(rightLocations).map((a, b) => Math.abs(a - b)).sum
+
+    print(answer)
+  }
+}
---
+++ src/main/scala/Day1Part2.scala
@@ -0,0 +1,15 @@
+import scala.io.Source
+import scala.util.Using
+
+object Day1Part2 {
+  def main(args: Array[String]): Unit = {
+    val input = Using(Source.fromResource("Day1.txt"))(_.getLines().toList).get
+    
+    val locations = input.collect { case s"$left   $right" => (left.toInt, right.toInt) }
+    val leftLocations = locations.map(_._1).groupBy(identity).map((number, list) => (number, list.size))
+    val rightLocations = locations.map(_._2).groupBy(identity).map((number, list) => (number, list.size))
+    val answer = leftLocations.map((number, count) => number * count * rightLocations.getOrElse(number, 0)).sum
+    
+    print(answer)
+  }
+}
