47d018b29f71635f55191393cbe571cf421872c2
Dylan Do Amaral <do.amaral.dylan@gmail.com>
2024-12-10 09:52:07 +0100

    feat: day 10 done

---
+++ src/main/scala/Day10Part2.scala
@@ -0,0 +1,36 @@
+import scala.io.Source
+import scala.util.Using
+
+object Day10Part2 {
+  def main(args: Array[String]): Unit = {
+    val input = Using(Source.fromResource("Day10.txt"))(_.getLines().toList).get
+    val map = input.toArray.map(line => line.toArray.map(_.toString.toInt))
+
+    val answer = computeScore(map)
+
+    print(answer)
+  }
+
+  def computeScore(map: Array[Array[Int]]): Int = {
+    val size = map.length
+
+    map.head.indices.flatMap { y =>
+      map.indices.map { x =>
+        if (map(x)(y) == 0) findTrails(map, size, x, y, 0).size
+        else 0
+      }
+    }.sum
+  }
+
+  def findTrails(map: Array[Array[Int]], size: Int, x: Int, y: Int, height: Int): List[(Int, Int)] = {
+    if (height == 9) List((x, y))
+    else {
+      List(
+        if (x - 1 >= 0 && map(x - 1)(y) == height + 1) findTrails(map, size, x - 1, y, height + 1) else List.empty,
+        if (y - 1 >= 0 && map(x)(y - 1) == height + 1) findTrails(map, size, x, y - 1, height + 1) else List.empty,
+        if (x + 1 < size && map(x + 1)(y) == height + 1) findTrails(map, size, x + 1, y, height + 1) else List.empty,
+        if (y + 1 < size && map(x)(y + 1) == height + 1) findTrails(map, size, x, y + 1, height + 1) else List.empty
+      ).flatten
+    }
+  }
+}
